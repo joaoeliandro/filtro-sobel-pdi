dbadf47b893da31379f2c387e6ea171bfb6948aa
João Eliandro <eliandrogermano@gmail.com>
2020-12-16 18:32:02 -0300

    Implemented scale gray functions

---
+++ src/scaleGray.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import open as openImage
+import os
+
+def grayScale(imageColored):
+  width, height = imageColored.size
+  image = Image.new("RGB", (width, height))
+
+  for x in range(width):
+    for y in range(height):
+      pixel = imageColored.getpixel((x, y))
+      luminous = (pixel[0] + pixel[1] + pixel[2])//3
+
+      image.putpixel((x, y), (luminous, luminous, luminous))
+
+  return image
+
+def grayScaleLevel(imageColored):
+  width, height = imageColored.size
+  image = Image.new("RGB", (width, height))
+
+  for x in range(width):
+    for y in range(height):
+      pixel = imageColored.getpixel((x, y))
+      luminous = int(0.3*pixel[0] + 0.59*pixel[1] + 0.11*pixel[2])
+
+      image.putpixel((x, y), (luminous, luminous, luminous))
+
+  return image
+
+if __name__ == "__main__":
+  womanImage = openImage.getImage("woman.jpg")
+  
+  print(womanImage.getpixel((100, 100)))
+  print(womanImage.getpixel((400, 200)))
+  print(womanImage.getpixel((300, 180)))
+
+  womanGray = grayScaleLevel(womanImage)
+  openImage.saveImage(womanGray, "womanGray2.jpg")
+  
