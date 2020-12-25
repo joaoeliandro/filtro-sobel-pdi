c2542803d34d159487d247b7c9a2d5ba3bb4dde5
João Eliandro <eliandrogermano@gmail.com>
2020-12-25 16:03:56 -0300

    Added showBoxBlur

--- src/filters.py
+++ src/filters.py
@@ -7,7 +7,15 @@ def imageFiltered(filename):
 
   openImage.showVertical(image, imageFiltered)
 
-def show_box_blur(filename, rImage = 1):
+def showBoxBlur(filename, rImage = 1):
+  originalImage = openImage.getImage(filename)
+  imageFiltered = originalImage.filter(ImageFilter.BoxBlur(rImage))
+
+  imageHorizontal = openImage.showHorizontal(originalImage, imageFiltered)
+  openImage.saveImage(imageHorizontal, '{}_boxblur_{}.png'
+    .format(filename[:filename.index('.')], rImage))
+  
+def showVertica(filename, rImage = 1):
   originalImage = openImage.getImage(filename)
   imageFiltered = originalImage.filter(ImageFilter.BoxBlur(rImage))
 
